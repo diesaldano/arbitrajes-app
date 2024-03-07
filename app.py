ac3a4f4ec20200be96e384dee71333f921c6ccfd
Diego Saldano <dsaldano@preminens.com.ar>
2024-03-07 16:43:37 -0300

    feat: modularize script, add scalping feature and cron tasks

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import os
 from dotenv import load_dotenv
 import requests
 
+#importar y cargar schudele luego lo separaremos en otro modulo
+import schedule
+import time
+
 # Carga las variables de entorno desde el archivo .env
 load_dotenv()
 
@@ -72,37 +76,21 @@ def procesar_datos(data_ci, data_48, tipo):
             precio_compra = ask_ci['price']
             if precio_compra > 0:
                 cantidad_maxima_comprable = min(saldo_disponible // precio_compra, ask_ci['size'], bid_48['size'])
-                if cantidad_maxima_comprable > 0:  # Asegúrate de que la cantidad máxima comprable sea mayor que cero
-                    ganancia_esperada = cantidad_maxima_comprable * diferencia_precios
-                    oportunidades_arbitraje.append({
-                        'tipo': tipo,
-                        'short_ticker': item_ci['short_ticker'],
-                        'instrument_name': item_ci['instrument_name'],
-                        'compra_ci_price': precio_compra,
-                        'venta_48_price': bid_48['price'],
-                        'cantidad': cantidad_maxima_comprable,
-                        'diferencia_precios': diferencia_precios,
-                        'ganancia_esperada': f"ARS {ganancia_esperada:.2f}"
-                    })
+            # if cantidad_maxima_comprable > 0:  # Asegúrate de que la cantidad máxima comprable sea mayor que cero
+                ganancia_esperada = 1000 * diferencia_precios
+                oportunidades_arbitraje.append({
+                    'tipo': tipo,
+                    'short_ticker': item_ci['short_ticker'],
+                    'instrument_name': item_ci['instrument_name'],
+                    'compra_ci_price': precio_compra,
+                    'venta_48_price': bid_48['price'],
+                    'cantidad': 1000,
+                    'diferencia_precios': diferencia_precios,
+                    'ganancia_esperada': f"ARS {ganancia_esperada:.2f}"
+                })
 
-# procesar_datos(data['general_ci'], data['general_48'], 'General')
-# procesar_datos(data['lideres_ci'], data['lideres_48'], 'Líderes')
-# procesar_datos(data['bonos_ci'], data['bonos_48'], 'Bonos')
-# procesar_datos(data['bonos_corp_ci'], data['bonos_corp_48'], 'Bonos Corporativos')
-
-# Imprime las oportunidades de arbitraje encontradas
-for oportunidad in oportunidades_arbitraje:
-    print(f"Oportunidad de arbitraje para {oportunidad['instrument_name']} (Ticker: {oportunidad['short_ticker']}, Tipo: {oportunidad['tipo']})")
-    print(f"\tCompra en CI a: {oportunidad['compra_ci_price']}")
-    print(f"\tVenta en 48hs a: {oportunidad['venta_48_price']}")
-    print(f"\tCantidad: {oportunidad['cantidad']}")
-    print(f"\tDiferencia de precios: {oportunidad['diferencia_precios']}")
-    print(f"\tGanancia esperada: {oportunidad['ganancia_esperada']}\n")
-
-oportunidades_probabilisticas = []
-
+# Función para procesar los datos y encontrar oportunidades de arbitraje probabilístico
 def procesar_datos_probabilisticos(data_ci, data_48, tipo):
-    oportunidades_probabilisticas = []
     for item_ci in data_ci['items']:
         matching_item = next((item_48 for item_48 in data_48['items'] if item_48['instrument_code'] == item_ci['instrument_code']), None)
         
@@ -117,45 +105,18 @@ def procesar_datos_probabilisticos(data_ci, data_48, tipo):
         
         spread = mejor_precio_compra_48 - precio_compra_probabilistico
         if spread > umbral_ganancia:
-            cantidad_maxima_comprable = min(saldo_disponible // precio_compra_probabilistico, matching_item['bids'][0]['size'])
-            ganancia_esperada = cantidad_maxima_comprable * spread
+            # cantidad_maxima_comprable = min(saldo_disponible // precio_compra_probabilistico, matching_item['bids'][0]['size'])
+            ganancia_esperada = 1000 * spread
             oportunidades_probabilisticas.append({
                 'tipo': tipo,
                 'short_ticker': item_ci['short_ticker'],
                 'instrument_name': item_ci['instrument_name'],
                 'compra_last_price': precio_compra_probabilistico,
                 'venta_bid_price_48': mejor_precio_compra_48,
-                'cantidad_comprable': cantidad_maxima_comprable,
+                'cantidad_comprable': 1000,
                 'diferencia_precios': spread,
                 'ganancia_esperada': f"ARS {ganancia_esperada:.2f}"
             })
-
-
-# Procesa los datos para general, líderes y bonos tanto para oportunidades certeras como probabilísticas
-for tipo in ['general', 'lideres', 'bonos', 'bonos_corp']:
-    procesar_datos(data[f'{tipo}_ci'], data[f'{tipo}_48'], tipo.capitalize())
-    procesar_datos_probabilisticos(data[f'{tipo}_ci'], data[f'{tipo}_48'], tipo.capitalize())
-
-# Imprime las oportunidades certeras de arbitraje encontradas
-print("Oportunidades Certeras de Arbitraje:")
-for oportunidad in oportunidades_arbitraje:
-    print(f"Oportunidad de arbitraje para {oportunidad['instrument_name']} (Ticker: {oportunidad['short_ticker']}, Tipo: {oportunidad['tipo']})")
-    print(f"\tCompra en CI a: {oportunidad['compra_ci_price']}")
-    print(f"\tVenta en 48hs a: {oportunidad['venta_48_price']}")
-    print(f"\tCantidad: {oportunidad['cantidad']}")
-    print(f"\tDiferencia de precios: {oportunidad['diferencia_precios']}")
-    # print(f"\tGanancia esperada: {oportunidad['ganancia_esperada']}\n")
-
-# Imprime las oportunidades probabilísticas de arbitraje encontradas
-print("Oportunidades Probabilísticas de Arbitraje:")
-for oportunidad in oportunidades_probabilisticas:
-    print(f"Oportunidad probabilística de arbitraje para {oportunidad['instrument_name']} (Ticker: {oportunidad['short_ticker']}, Tipo: {oportunidad['tipo']})")
-    print(f"\tÚltimo precio vendido en CI: {oportunidad['compra_last_price']}")
-    print(f"\tÚltimo precio ofrecido en 48hs: {oportunidad['venta_ask_price']}")
-    print(f"\tDiferencia de precios: {oportunidad['diferencia_precios']}\n")
-    # print(f"\tGanancia esperada: {oportunidad['ganancia_esperada']}\n")
-    
-
 
 def buscar_oportunidades_dolar_mep(data_ars, data_usd, tipo):
     for item_ars in data_ars['items']:
@@ -166,24 +127,133 @@ def buscar_oportunidades_dolar_mep(data_ars, data_usd, tipo):
         if item_ars['ask'] and item_usd['bid']:
             tipo_cambio_mep = item_ars['ask'] / item_usd['bid']
             ganancia_usd = saldo_disponible / tipo_cambio_mep
-            oportunidades_mep.append({
-                'tipo': tipo,
-                'instrument_code': item_ars['instrument_code'],
-                'compra_ars': item_ars['ask'],
-                'venta_usd': item_usd['bid'],
-                'tipo_cambio_mep': tipo_cambio_mep,
-                'ganancia_usd': ganancia_usd                                                    
+            if tipo_cambio_mep < 1000:
+                oportunidades_mep.append({
+                    'tipo': tipo,
+                    'instrument_code': item_ars['instrument_code'],
+                    'compra_ars': item_ars['ask'],
+                    'venta_usd': item_usd['bid'],
+                    'tipo_cambio_mep': tipo_cambio_mep,
+                    'ganancia_usd': ganancia_usd                                                    
+                })
+
+def imprimir_oportunidades():
+    for oportunidad in oportunidades_arbitraje:
+        print(f"Oportunidad de arbitraje para {oportunidad['instrument_name']} (Ticker: {oportunidad['short_ticker']}, Tipo: {oportunidad['tipo']})")
+        print(f"\tCompra en CI a: {oportunidad['compra_ci_price']}")
+        print(f"\tVenta en 48hs a: {oportunidad['venta_48_price']}")
+        print(f"\tCantidad: {oportunidad['cantidad']}")
+        print(f"\tDiferencia de precios: {oportunidad['diferencia_precios']}")
+        print(f"\tGanancia esperada: {oportunidad['ganancia_esperada']}\n")
+        
+    # Imprime las oportunidades certeras de arbitraje encontradas
+    print("Oportunidades Certeras de Arbitraje:")
+    for oportunidad in oportunidades_arbitraje:
+        print(f"Oportunidad de arbitraje para {oportunidad['instrument_name']} (Ticker: {oportunidad['short_ticker']}, Tipo: {oportunidad['tipo']})")
+        print(f"\tCompra en CI a: {oportunidad['compra_ci_price']}")
+        print(f"\tVenta en 48hs a: {oportunidad['venta_48_price']}")
+        print(f"\tCantidad: {oportunidad['cantidad']}")
+        print(f"\tDiferencia de precios: {oportunidad['diferencia_precios']}")
+        # print(f"\tGanancia esperada: {oportunidad['ganancia_esperada']}\n")
+
+    # Imprime las oportunidades probabilísticas de arbitraje encontradas
+    print("Oportunidades Probabilísticas de Arbitraje:")
+    for oportunidad in oportunidades_probabilisticas:
+        print(f"Oportunidad probabilística de arbitraje para {oportunidad['instrument_name']} (Ticker: {oportunidad['short_ticker']}, Tipo: {oportunidad['tipo']})")
+        print(f"\tÚltimo precio vendido en CI: {oportunidad['compra_last_price']}")
+        print(f"\tÚltimo precio ofrecido en 48hs: {oportunidad['venta_bid_price_48']}")
+        print(f"\tDiferencia de precios: {oportunidad['diferencia_precios']}\n")
+        # print(f"\tGanancia esperada: {oportunidad['ganancia_esperada']}\n")
+
+    # Imprimir las oportunidades encontradas
+    print("Oportunidades Dólar MEP:")
+    for oportunidad in oportunidades_mep:
+        print(f"Oportunidad de Dólar MEP en {oportunidad['tipo']} ({oportunidad['instrument_code']})")
+        print(f"\tCompra en ARS: {oportunidad['compra_ars']}")
+        print(f"\tVenta en USD: {oportunidad['venta_usd']}")
+        print(f"\tTipo de cambio MEP: {oportunidad['tipo_cambio_mep']:.2f}\n")
+        # print(f"\tGanancia USD: {oportunidad['ganancia_usd']}")
+        
+def procesar_scalping(data_ci):
+    oportunidades_scalping = []
+    for item in data_ci['items']:
+        if not item.get('asks') or not item.get('bids'):
+            continue
+
+        # Consideramos el mejor precio de venta (ask) y el mejor precio de compra (bid)
+        mejor_ask = item['asks'][0]
+        mejor_bid = item['bids'][0]
+
+        # Verificamos que exista una diferencia de precio que justifique el scalping
+        diferencia_precios = mejor_ask['price'] - mejor_bid['price']
+        if diferencia_precios <= 0 or (mejor_ask['size'] < 1 or mejor_bid['size'] < 1):
+            continue
+
+        # Comprobamos la tendencia del mercado para ese instrumento
+        tendencia = (item['last'] - item['prev_close']) / item['prev_close'] if item['prev_close'] else 0
+
+        # Consideramos solo oportunidades con tendencia positiva y con volumen significativo
+        if tendencia > 0 and item['volume'] > 1000:  # El umbral de volumen puede ajustarse según el análisis
+            oportunidades_scalping.append({
+                'ticker': item['short_ticker'],
+                'nombre': item['instrument_name'],
+                'precio_compra': mejor_bid['price'],
+                'precio_venta': mejor_ask['price'],
+                'volumen': item['volume'],
+                'diferencia_precios': diferencia_precios,
+                'tendencia': tendencia
             })
 
-# Ejecutar la búsqueda de oportunidades para cada par de bonos/ON en ARS y USD
-buscar_oportunidades_dolar_mep(data['bonos_ci'], data['bonos_usd_48'], 'Bonos Públicos MEP')
-buscar_oportunidades_dolar_mep(data['bonos_corp_ci'], data['bonos_corp_usd_48'], 'Bonos Corporativos MEP')
+    return oportunidades_scalping
 
-# Imprimir las oportunidades encontradas
-print("Oportunidades Dólar MEP:")
-for oportunidad in oportunidades_mep:
-    print(f"Oportunidad de Dólar MEP en {oportunidad['tipo']} ({oportunidad['instrument_code']})")
-    print(f"\tCompra en ARS: {oportunidad['compra_ars']}")
-    print(f"\tVenta en USD: {oportunidad['venta_usd']}")
-    print(f"\tTipo de cambio MEP: {oportunidad['tipo_cambio_mep']:.2f}\n")
-    # print(f"\tGanancia USD: {oportunidad['ganancia_usd']}")
+def imprimir_oportunidades_scalping(oportunidades_scalping):
+    for oportunidad in oportunidades_scalping:
+        print(f"Oportunidad de Scalping para {oportunidad['nombre']} (Ticker: {oportunidad['ticker']})")
+        print(f"\tCompra a: {oportunidad['precio_compra']}")
+        print(f"\tVenta a: {oportunidad['precio_venta']}")
+        print(f"\tVolumen: {oportunidad['volumen']}")
+        print(f"\tDiferencia de precios: {oportunidad['diferencia_precios']}")
+        print(f"\tTendencia: {'Positiva' if oportunidad['tendencia'] > 0 else 'Negativa'}\n")
+
+def main():
+    print("Inicializando la aplicación...")
+    
+    # Procesa los datos para general, líderes y bonos tanto para oportunidades certeras como probabilísticas
+    for tipo in ['general', 'lideres', 'bonos', 'bonos_corp']:
+        procesar_datos(data[f'{tipo}_ci'], data[f'{tipo}_48'], tipo.capitalize())
+        procesar_datos_probabilisticos(data[f'{tipo}_ci'], data[f'{tipo}_48'], tipo.capitalize())
+
+    # Ejecutar la búsqueda de oportunidades para cada par de bonos/ON en ARS y USD
+    buscar_oportunidades_dolar_mep(data['bonos_ci'], data['bonos_usd_48'], 'Bonos Públicos MEP')
+    buscar_oportunidades_dolar_mep(data['bonos_corp_ci'], data['bonos_corp_usd_48'], 'Bonos Corporativos MEP')
+
+    # Imprime las oportunidades encontradas
+    imprimir_oportunidades()
+    
+    print("Esperando 5 minutos para la próxima ejecución...")
+    # imprimir hora de ejecución
+    print(time.strftime("%H:%M:%S"))
+
+def scalping():
+    print("Inicializando la búsqueda de oportunidades de scalping...")
+    oportunidades_scalping_total = []  # Lista para guardar todas las oportunidades de scalping
+    for tipo in ['general', 'lideres', 'bonos', 'bonos_corp']:
+        oportunidades_scalping = procesar_scalping(data[f'{tipo}_ci'])  # Obtiene las oportunidades de scalping
+        oportunidades_scalping_total.extend(oportunidades_scalping)  # Agrega las oportunidades encontradas a la lista total
+
+    imprimir_oportunidades_scalping(oportunidades_scalping_total)  # Ahora pasamos la lista correcta como argumento
+    print("Esperando 60 segundos para la próxima ejecución...")
+    # imprimir hora de ejecución
+    print(time.strftime("%H:%M:%S"))
+
+main()
+scalping()
+
+
+# Ejecutar la función main cada 5 minutos
+schedule.every(5).minutes.do(main)
+schedule.every(60).seconds.do(scalping)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
